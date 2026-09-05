e0ea2a590a129ae616f6b6fa62f0eab759154e78
mugata <dltnals0109@naver.com>
2026-09-06 05:16:06 +0900

    reset-button

--- main.js
+++ main.js
@@ -13,9 +13,10 @@ let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 // console.log("버튼", playButton);
 let resultArea = document.getElementById("result-area");
-console.log(resultArea);
+let resetButton = document.getElementById("reset-button");
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -34,4 +35,11 @@ function play() {
   }
 }
 
+function reset() {
+  // user input창이 깨끗하게 정리되고
+  userInput.value = "";
+  //   새로운 번호가 생성
+  pickRandomNum();
+  resultArea.textContent = "결과값이 여기 나옵니다.!!";
+}
 pickRandomNum();
